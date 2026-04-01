618d960d87d9bd334bfc6f71c03a89aaba134876
42712 <115371094+42712@users.noreply.github.com>
2026-04-01 17:38:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+from flask import Flask
 from analyzer import analisar
 from trader import comprar
 from config import MAX_TRADES
+import threading
 import time
+
+app = Flask(__name__)
 
 ativos = []
 
-while True:
-    sinais = analisar()
+def bot_loop():
+    while True:
+        try:
+            sinais = analisar()
 
-    for moeda in sinais:
-        if moeda not in ativos and len(ativos) < MAX_TRADES:
-            print("🔥 ENTRADA:", moeda)
-            comprar(moeda)
-            ativos.append(moeda)
+            for moeda in sinais:
+                if moeda not in ativos and len(ativos) < MAX_TRADES:
+                    print("🔥 ENTRADA:", moeda)
+                    comprar(moeda)
+                    ativos.append(moeda)
 
-    print("⏳ aguardando próxima rodada...")
-    time.sleep(300)
+            print("⏳ aguardando...")
+            time.sleep(300)
+
+        except Exception as e:
+            print("Erro:", e)
+            time.sleep(60)
+
+@app.route("/")
+def home():
+    return "🤖 MEGATRON BOT ONLINE"
+
+if __name__ == "__main__":
+    t = threading.Thread(target=bot_loop)
+    t.start()
+
+    app.run(host="0.0.0.0", port=10000)
